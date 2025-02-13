b46580f99891369ffd5560814fe75410719b536b
Cyr2 <cyrigamingpro@gmail.com>
2025-02-13 12:33:29 +0100

    Init projet

--- public/index.php
+++ public/index.php
@@ -11,42 +11,5 @@ $router = new App\Router($_SERVER["REQUEST_URI"]);
 $router->get('/', 'HomeController@index');
 
 $router->get('/login', 'UserController@showLogin');
-$router->get('/register', 'UserController@showRegister');
-$router->post('/register', 'UserController@register');
-$router->post('/login', 'UserController@login');
-$router->get('/logout', 'UserController@logout');
-
-$router->get('/travel', 'HomeController@travel');
-$router->get('/travel/:id', 'OrderController@reservation');
-$router->post('/travel/:id', 'OrderController@reservationConfirm');
-
-$router->get('/filtred/:id', 'HomeController@filtredTravel');
-
-$router->post('/confirmation', 'OrderController@confirmation');
-
-$router->get('/voyages', 'HomeController@voyages');
-
-$router->get('/admin', 'AdminController@dashboard');
-
-$router->get('/admin/updateTravel/:id', 'AdminController@updateTravel');
-$router->post('/admin/updateTravel/:id', 'AdminController@updateTravel');
-$router->get('/admin/deleteTravel/:id', 'AdminController@deleteTravel');
-
-$router->get('/admin/updateCategory/:id', 'AdminController@updateCategory');
-$router->post('/admin/updateCategory/:id', 'AdminController@updateCategory');
-$router->get('/admin/deleteCategory/:id', 'AdminController@deleteCategory');
-$router->get('/admin/addCategory', 'AdminController@addCategory');
-$router->post('/admin/addCategory', 'AdminController@addCategory');
-
-$router->get('/admin/updateUser/:id', 'AdminController@updateUser');
-$router->post('/admin/updateUser/:id', 'AdminController@updateUser');
-$router->get('/admin/deleteUser/:id', 'AdminController@deleteUser');
-
-$router->get('/admin/updateOrder/:id', 'AdminController@updateOrder');
-$router->post('/admin/updateOrder/:id', 'AdminController@updateOrder');
-$router->get('/admin/deleteOrder/:id', 'AdminController@deleteOrder');
-
-$router->get('/admin/addTravel', 'AdminController@addTravel');
-$router->post('/admin/addTravel', 'AdminController@addTravel');
 
 $router->run();
--- resources/views/content/homepage.php
+++ resources/views/content/homepage.php
@@ -1,9 +1,11 @@
 <?php ob_start(); ?>
 
-    <section class="flex flex-col">
-      <img src="./img/caraibes1.jpg" alt="hero" class="max-h-[70vh] min-w-screen relative left-[-20vw] scale-y-125 -translate-y-1/4 top-0 left-0 z-5" />
-      <a href="/travel" class="w-full p-2 border-cyan-500 border-2 rounded-md cursor-pointer text-center hover:bg-cyan-500 hover:text-white">Choisir mon séjour tout de suite</a>
-    </section>
+<section>
+    <form method="POST">
+        <label>Category Name: <input type="text" name="name_category" required></label><br>
+        <button type="submit">Add Category</button>
+    </form>
+</section>
 
 <?php $content = ob_get_clean(); ?>
 <?php require VIEWS . 'layout.php'; ?>
